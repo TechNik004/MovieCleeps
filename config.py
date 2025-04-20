29ca6c201a9b2906fcb3f43b5d3760daeb067a12
Nikhil792 <114512871+Nikhil792@users.noreply.github.com>
2025-04-20 12:23:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
 import re
 import os
 from os import environ
@@ -22,7 +17,7 @@ API_ID = int(environ.get("API_ID", "10671416"))
 API_HASH = environ.get("API_HASH", "d32e7a9253928d78d7888c0b9998939d")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://envs.sh/YJ1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "AnimeTrexBot") # without @
 PORT = environ.get("PORT", "8080")
