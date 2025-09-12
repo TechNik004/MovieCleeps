57e16457477a5fbdfb27d693a15c12700d9af524
Nikhil792 <114512871+Nikhil792@users.noreply.github.com>
2025-09-12 10:33:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://envs.sh/YJ1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "AnimeTrexBot") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "FileShare_newBot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
