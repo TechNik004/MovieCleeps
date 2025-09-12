6ee651842a2d5bbe7254987dd36d4a9375f3e749
Nikhil792 <114512871+Nikhil792@users.noreply.github.com>
2025-09-12 10:34:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ API_HASH = environ.get("API_HASH", "d32e7a9253928d78d7888c0b9998939d")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://envs.sh/YJ1.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6667067260').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "FileShare_newBot") # without @
 PORT = environ.get("PORT", "8080")
 
