cb5fe18db577981acc8ad448a83520564daaef8b
TechNik004 <gorka8212@gmail.com>
2025-10-08 18:17:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -65,7 +65,7 @@ WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or Fa
 WEBSITE_URL = environ.get("WEBSITE_URL", "https://nikdemon.blogspot.com/2025/04/nik-trex.html") # For More Information Check Video On Yt - @Tech_VJ
 
 # File Stream Config
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
+STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
 
 # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
